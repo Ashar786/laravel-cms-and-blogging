fd9a134cf33587ff48ee815d4b37d59113fa88af
Ashar Khan <0ashar0786@gmail.com>
2018-04-14 14:55:44 +0530

    Created postsController and admin>post>create view.

---
+++ resources/views/admin/post/create.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h2 class="text-center">
+        Create Post
+    </h2>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/post/create', [
+    'uses' => 'postsController@create',
+    'as' => 'post.create'
+]);
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
